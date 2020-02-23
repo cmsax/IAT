6f073c70d62fe0a892c45fd7eceb5f9cfa4527c7
蔡明师 <i@unoiou.com>
2020-02-23 20:55:56 +0800

    add transition

--- spa/iat/src/App.vue
+++ spa/iat/src/App.vue
@@ -1,14 +1,25 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <transition :name="transitionName">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
-<script>
-export default {
-  name: "app",
-  components: {}
-};
+<script lang="ts">
+import { Vue, Component, Watch } from "vue-property-decorator";
+import { Route } from "vue-router";
+@Component({})
+export default class App extends Vue {
+  private transitionName = "";
+
+  @Watch("$route")
+  onRouteChange(to: Route, from: Route) {
+    const toDepth = to.path.split("/").length;
+    const fromDepth = from.path.split("/").length;
+    this.transitionName = toDepth < fromDepth ? "slide-right" : "slide-left";
+  }
+}
 </script>
 
 <style>
