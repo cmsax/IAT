491083762fbf683d85262e078336b05028b64643
蔡明师 <i@unoiou.com>
2020-02-24 18:56:23 +0800

    fuck sqlalchemy

--- serverless/iat/index.py
+++ serverless/iat/index.py
@@ -1,46 +1,18 @@
 # -*- coding: utf8 -*-
-import sqlalchemy as db
+import pymysql.cursors
 
 from datetime import datetime
 from os import getenv
-from sqlalchemy import BIGINT, Column, JSON, String, TIMESTAMP
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
 
-Base = declarative_base()
-engine = db.create_engine(
-    (
-        'mysql+mysqlconnector://{user}:{password}@{host}:{port}/{db}'
-        '?auth_plugin=mysql_native_password'
-    ).format(
-        host=getenv("MYSQL_HOST"),
-        port=getenv("MYSQL_PORT"),
-        password=getenv("MYSQL_USER_PWD"),
-        user=getenv("MYSQL_USER"),
-        db=getenv("MYSQL_DB")
-    )
+connection = pymysql.connect(
+    host=getenv("MYSQL_HOST"),
+    port=int(getenv("MYSQL_PORT")),
+    password=getenv("MYSQL_USER_PWD"),
+    user=getenv("MYSQL_USER"),
+    db=getenv("MYSQL_DB"),
+    charset='utf8',
+    cursorclass=pymysql.cursors.DictCursor
 )
-engine.connect()
-Base = declarative_base()
-
-
-class IAT(Base):
-    __tablename__ = 'iat'
-    id = Column(BIGINT, primary_key=True, autoincrement=True, nullable=True)
-    ip = Column(String)
-    user_agent = Column(String)
-    submit_date = Column(TIMESTAMP, nullable=True, default=datetime.now())
-    result = Column(JSON)
-
-
-session_maker = sessionmaker(bind=engine)
-s = session_maker()
-
-
-def save_result(body, ip, ua):
-    result = IAT(ip=ip, user_agent=ua, result=body)
-    s.add(result)
-    s.commit()
 
 
 def main_handler(event, context):
@@ -49,6 +21,12 @@ def main_handler(event, context):
 
     body = event['body']
     ip = event['requestContext']['sourceIp']
-    ua = event['headers']['User-Agent']
-    save_result(body, ip, ua)
-    return {'msg': 'ok'}
+    ua = event['headers'].get('User-Agent', 'null')
+    with connection.cursor() as cursor:
+        sql = "INSERT INTO `iat` (`ip`, `submit_date`, `user_agent`, `result`) VALUES (%s, %s, %s, %s)"
+        cursor.execute(
+            sql, (ip, datetime.now(), ua, body)
+        )
+    connection.commit()
+
+    return {'msg': 'ok, thanks'}
