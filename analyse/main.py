c86a742a9724aaeab41ff6a85daef4ebe52a1916
蔡明师 <cmsax@live.com>
2020-03-02 16:09:00 +0800

    update

--- analyse/main.py
+++ analyse/main.py
@@ -3,6 +3,7 @@
 # File: analyse/main.py
 # Author: MingshiCai i@unoiou.com
 # Date: 2020-03-01 14:21:56
+import copy
 import json
 import statistics
 import pandas as pd
@@ -11,6 +12,42 @@ from scipy.stats import ttest_ind
 
 positive_words = ["美味的", "可口的", "诱人的", "吸引人的", "令人垂涎的"]
 negative_words = ["乏味的", "不吸引人的", "无味的", "不好吃的", "不美味的"]
+
+
+def mean_elapsed(row):
+    """calculate row's mean elapsed time
+    """
+    all_tests = []
+    for tp in row['result']['finishedTestPacks']:
+        tests_in_pack = tp['finishedTests']
+        tests_fixed_error = fix_error(tests_in_pack)
+        all_tests.extend(tests_fixed_error)
+    return statistics.mean(test['elapsed'] for test in all_tests)
+
+
+def overall_stdev_mean(rows_):
+    """calculate overall stdev.
+    """
+    rows = copy.deepcopy(rows_)
+    all_tests = []
+    for row in rows:
+        row['result'] = json.loads(row['result'])
+        for tp in row['result']['finishedTestPacks']:
+            tests_in_pack = tp['finishedTests']
+            tests_fixed_error = fix_error(tests_in_pack)
+            all_tests.extend(tests_fixed_error)
+
+    series = [test['elapsed'] for test in all_tests]
+    return statistics.stdev(series), statistics.mean(series)
+
+
+def out_of_range(row_mean, all_stdev, all_mean):
+    """Decide whether row val out of range.
+    """
+    if row_mean > all_mean + 3 * all_stdev or row_mean < all_mean - 3 * all_stdev:
+        return True
+    else:
+        return False
 
 
 def gen_test_pack_id(test_pack):
@@ -87,10 +124,30 @@ result = None
 with open('./result.json', 'r+') as f:
     result = json.load(f)['RECORDS']
 
-skipped_rows = []
+skipped_rows = {
+    'dysopia': [],
+    'error': [],
+    'outofrange': [],
+    'leave': [],
+}
+
 final_results = []
 simple_results = []
 stats = []
+
+series_mapping = {
+    4: {
+        'series': [],
+        'variance': None
+    },
+    7: {
+        'series': [],
+        'variance': None
+    }
+}
+
+
+all_stdev, all_mean = overall_stdev_mean(result)
 
 for row in result:
     row['result'] = json.loads(row['result'])
@@ -100,9 +157,27 @@ for row in result:
         test_pack for test_pack in row['result']['finishedTestPacks']
     ])):
         all_cases.extend(cases)
+
+    # remove error
     ratio = err_ratio(all_cases)
     if ratio >= 0.35:
-        skipped_rows.append(row)
+        skipped_rows['error'].append(row)
+        continue
+
+    # remove dysopia
+    if row['result']['userInfo']['dysopia'] != 'none':
+        skipped_rows['dysopia'].append(row)
+        continue
+
+    # remove out of range
+    row_mean = mean_elapsed(row)
+    if out_of_range(row_mean, all_stdev, all_mean):
+        skipped_rows['outofrange'].append(row)
+        continue
+
+    # remove leave
+    if row['result']['userLeaveSpans']:
+        skipped_rows['leave'].append(row)
         continue
 
     # each subject's columns
@@ -115,6 +190,13 @@ for row in result:
 
         if id_ in [4, 7]:
             series = [test['elapsed'] for test in fixed_tests]
+            series_mapping[id_]['series'].extend(series)
+            # series = list(filter(
+            #     # lambda x: x > 300, [
+            #     lambda x: x < 20000 and x > 300, [
+            #         test['elapsed'] for test in fixed_tests
+            #     ]
+            # ))
             mean = statistics.mean(series)
             stdev = statistics.stdev(series)
             variance = statistics.variance(series)
@@ -228,3 +310,38 @@ p7_series = final_stats['p7-sum'].values
 
 t, p = ttest_ind(p4_series, p7_series)
 print(t, p)
+
+p4_mean = final_stats['p4-sum'].mean()
+p7_mean = final_stats['p7-sum'].mean()
+series = []
+series.extend(final_stats['p4-sum'].values.tolist())
+series.extend(final_stats['p4-sum'].values.tolist())
+stdev = statistics.stdev(series)
+dscore = (p7_mean - p4_mean) / stdev
+
+print(dscore)
+
+series_mapping[4]['variance'] = statistics.variance(
+    series_mapping[4]['series']
+)
+series_mapping[7]['variance'] = statistics.variance(
+    series_mapping[7]['series']
+)
+
+
+extra_result = {
+    'variance': {
+        'p4': series_mapping[4]['variance'],
+        'p7': series_mapping[7]['variance'],
+    },
+    'dscore': dscore,
+    'ttest': {
+        't': t,
+        'p': p
+    }
+}
+
+with open('extra_result.json', 'w+') as f:
+    json.dump(extra_result, f)
+
+print('done')
